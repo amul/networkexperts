dd0fbddfebf2e1226795c60369208b4a1ff8a299
amul <amulp84@gmail.com>
2015-02-15 03:16:33 +0000

    add home controller and root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root to: 'home#index'
+root to: 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
